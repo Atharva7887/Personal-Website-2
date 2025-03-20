bfde2fec7ee15093467326b5e7ab7df006af8831
Atharva Shirke <77715330+Atharva7887@users.noreply.github.com>
2025-03-20 18:08:54 +0530

    Update Card3D.tsx

--- src/components/Card3D.tsx
+++ src/components/Card3D.tsx
@@ -1,20 +1,19 @@
 import { Github, Instagram, Linkedin } from "lucide-react";
 
-
 export default function Card3D() {
   return (
     <div className="card">
       <div className="image-container">
-        <img src="https://raw.githubusercontent.com/Atharva7887/Personal-Website-2/main/src/components/Images/me.jpg" alt="My Image is not visible because i have got n bill from AWS :(" className="card-image" />
+        <img
+          src="https://raw.githubusercontent.com/Atharva7887/Personal-Website-2/main/src/components/Images/me.jpg"
+          alt="My Image is not visible because I have got n bill from AWS :("
+          className="card-image"
+        />
       </div>
-    </div>
-  );
-}
 
-      
       <p className="heading">Atharva</p>
       <p>A Computer Engineer</p>
-      
+
       <div className="social-icons">
         <a
           href="https://github.com/Atharva7887"
